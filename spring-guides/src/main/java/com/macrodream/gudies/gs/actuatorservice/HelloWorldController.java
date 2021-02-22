e542b3deba5f618b0fe682febeef95199a728905
macros <macroszhang@yeah.net>
2021-02-22 15:53:46 +0800

    add actuatorservice test

--- spring-guides/src/main/java/com/macrodream/gudies/gs/actuatorservice/HelloWorldController.java
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/actuatorservice/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
     @ResponseBody
     public Greeting sayHello(@RequestParam(name = "name", required = false,
         defaultValue = "Stranger") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
 
 }
