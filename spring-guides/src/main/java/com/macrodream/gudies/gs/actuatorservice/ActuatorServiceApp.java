5a62f091b1487cc980bc83cacb2706369cd2545d
macros <macroszhang@yeah.net>
2021-02-22 15:35:13 +0800

    add actuatorservice

---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/actuatorservice/ActuatorServiceApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.gudies.gs.actuatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorServiceApp.class,args);
+    }
+}
