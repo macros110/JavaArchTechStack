c625e66efba9257eba1b9f47aeb1ef4c3089eb4e
macros <macroszhang@yeah.net>
2021-02-24 17:07:30 +0800

    add hateoas demo test

---
+++ spring-guides/src/test/java/com/macrodream/gudies/gs/resthateoas/GreetMockMvcTest.java
@@ -0,0 +1,26 @@
+package com.macrodream.gudies.gs.resthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetMockMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void envEndpointNotHidden() throws Exception{
+        mockMvc.perform(get("/greeting"))
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+}
