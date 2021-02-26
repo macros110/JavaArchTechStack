c5d7981f95cf30fd25761eaf9f2764a915242214
macros <macroszhang@yeah.net>
2021-02-26 14:25:14 +0800

    add managing transaction

---
+++ spring-guides/build/resources/main/schema.sql
@@ -0,0 +1,3 @@
+--Managing Transactions
+drop table BOOKINGS if exists;
+create table BOOKINGS(ID serial, FIRST_NAME varchar(5) NOT NULL);
---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/managingtransactions/ManagingTransactionsApp.java
@@ -0,0 +1,14 @@
+package com.macrodream.gudies.gs.managingtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Managing Transactions
+ */
+@SpringBootApplication
+public class ManagingTransactionsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingTransactionsApp.class, args);
+    }
+}
