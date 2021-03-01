8d9b927ad85529775d8de73698a06188427ccc26
macros <macroszhang@yeah.net>
2021-03-01 17:16:01 +0800

    add messagingstopwebsocket

---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,17 @@
+package com.macrodream.gudies.gs.messagingstompwebsocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/messagingstompwebsocket/MessagingStompWebsocketApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.gudies.gs.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebsocketApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingStompWebsocketApp.class,args);
+    }
+}
