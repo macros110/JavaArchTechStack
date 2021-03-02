810d7d12630159b896e6d387263e831000434f0d
macros <macroszhang@yeah.net>
2021-03-02 14:13:22 +0800

    add restservicecors

--- spring-guides/src/main/java/com/macrodream/gudies/gs/messagingstompwebsocket/MessagingStompWebsocketApp.java
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/messagingstompwebsocket/MessagingStompWebsocketApp.java
@@ -3,6 +3,9 @@ package com.macrodream.gudies.gs.messagingstompwebsocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Using WebSocket to build an interactive web application
+ */
 @SpringBootApplication
 public class MessagingStompWebsocketApp {
     public static void main(String[] args) {
