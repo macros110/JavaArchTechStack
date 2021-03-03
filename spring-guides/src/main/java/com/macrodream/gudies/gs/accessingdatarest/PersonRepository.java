9dad30cc99f51db3c852bcdf9cec2efd382b50e5
macros <macroszhang@yeah.net>
2021-03-03 12:39:53 +0800

    add accessingdatarest

---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/accessingdatarest/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.macrodream.gudies.gs.accessingdatarest;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person,Long> {
+    List<Person> findByLastName(@Param("name") String name);
+}
