7f4c24aa3f0cc662ad4c1c5e9bfa96e09828d9b5
macros <macroszhang@yeah.net>
2021-03-10 07:52:35 +0800

    add accessingmongodbdatarest

---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/accessingmongodbdatarest/AccessingMongodbDataRestApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.gudies.gs.accessingmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongodbDataRestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingMongodbDataRestApp.class,args);
+    }
+}
