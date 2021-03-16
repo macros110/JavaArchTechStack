00493330e516fd735c3f41457d2e7f9a529773c5
macros <macroszhang@yeah.net>
2021-03-17 07:56:22 +0800

    add producingwebservice with error

---
+++ spring-guides/src/main/java/com/macrodream/gudies/gs/producingwebservice/ProductWebServiceApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.gudies.gs.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWebServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductWebServiceApp.class, args);
+    }
+}
