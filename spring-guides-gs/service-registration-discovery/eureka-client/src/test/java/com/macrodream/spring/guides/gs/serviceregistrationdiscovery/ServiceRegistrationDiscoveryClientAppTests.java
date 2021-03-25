3a364a634ba93f82503b638b21257f494cd691e7
macros <macroszhang@yeah.net>
2021-03-25 22:52:18 +0800

    add eureka service & client

---
+++ spring-guides-gs/service-registration-discovery/eureka-client/src/test/java/com/macrodream/spring/guides/gs/serviceregistrationdiscovery/ServiceRegistrationDiscoveryClientAppTests.java
@@ -0,0 +1,12 @@
+package com.macrodream.spring.guides.gs.serviceregistrationdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceRegistrationDiscoveryClientAppTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
