deecaec712a56d47ac54536e093019bbe2d13b4f
macros <macroszhang@yeah.net>
2021-04-13 14:10:09 +0800

    add testing restdocs

---
+++ spring-guides-gs/testing-restdocs/src/test/java/com/macrodream/spring/guides/gs/testingrestdocs/TestingTestRestDocsAppTests.java
@@ -0,0 +1,12 @@
+package com.macrodream.spring.guides.gs.testingrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingTestRestDocsAppTests {
+    @Test
+    public void contextLoads() throws Exception{
+
+    }
+}
