a54fd2cb425c150bb636e8a90fada5a8695ec224
macros <macroszhang@yeah.net>
2021-05-05 12:08:26 +0800

    add accessing data reactive redis

---
+++ spring-guides-gs/accessing-data-reative-redis/src/main/java/com/macrodream/spring/guides/gs/accessingdatareactiveredis/AccessingDataReactiveRedisApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.spring.guides.gs.accessingdatareactiveredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataReactiveRedisApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataReactiveRedisApp.class, args);
+    }
+}
