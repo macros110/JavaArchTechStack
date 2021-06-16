f6e29bda8d082ce7acbea27e91db3d022767746a
macros <macroszhang@yeah.net>
2021-06-16 11:46:46 +0800

    add securing-web

---
+++ spring-guides-gs/securing-web/src/main/java/com/macrodream/spring/guides/gs/securingweb/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.macrodream.spring.guides.gs.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * The web application is based on Spring MVC.
+ * As a result, you need to configure Spring MVC
+ * and set up view controllers to expose these templates.
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ spring-guides-gs/securing-web/src/main/java/com/macrodream/spring/guides/gs/securingweb/SecuringWebApp.java
@@ -0,0 +1,11 @@
+package com.macrodream.spring.guides.gs.securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringWebApp.class, args);
+    }
+}
