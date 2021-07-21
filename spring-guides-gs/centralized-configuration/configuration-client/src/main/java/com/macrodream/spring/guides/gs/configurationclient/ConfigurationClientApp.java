9be480409bad98b079bc4651aa36a0813fb8420c
macros <macroszhang@yeah.net>
2021-07-21 23:29:25 +0800

    add centralized-configuration

---
+++ spring-guides-gs/centralized-configuration/configuration-client/src/main/java/com/macrodream/spring/guides/gs/configurationclient/ConfigurationClientApp.java
@@ -0,0 +1,26 @@
+package com.macrodream.spring.guides.gs.configurationclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ConfigurationClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationClientApp.class, args);
+    }
+}
+@RefreshScope
+@RestController
+class MessageRestController{
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage(){
+        return this.message;
+    }
+}
