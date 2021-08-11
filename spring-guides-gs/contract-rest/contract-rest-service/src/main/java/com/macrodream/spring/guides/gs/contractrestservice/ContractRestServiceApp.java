8aeccb4c11645b6e8aa4d48146547127ea94450f
macros <macroszhang@yeah.net>
2021-08-11 18:22:51 +0800

    20210811 add contract-rest

---
+++ spring-guides-gs/contract-rest/contract-rest-service/src/main/java/com/macrodream/spring/guides/gs/contractrestservice/ContractRestServiceApp.java
@@ -0,0 +1,12 @@
+package com.macrodream.spring.guides.gs.contractrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractRestServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractRestServiceApp.class, args);
+    }
+}
