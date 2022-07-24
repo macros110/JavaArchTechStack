369f145bbfa05ce41288d03178e44ebc49826936
macros <macroszhang@yeah.net>
2022-07-24 21:13:58 +0800

    20220717 AOP原理总结

--- tech-stack/spring-annotation/src/main/java/com/macrodream/config/MainConfigOfAop.java
+++ tech-stack/spring-annotation/src/main/java/com/macrodream/config/MainConfigOfAop.java
@@ -152,6 +152,25 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *                      2) 链式获取每个拦截器，拦截器执行invoke方法，每个拦截器等待下一个拦截器执行完成返回以后再来执行；
  *                          拦截器链的机制，保证通知方法与目标方法的执行顺序 aopMethodInterceptor.png
  *
+ *  AOP总结：
+ *      1 @EnableAspectJAutoProxy 开启AOP功能
+ *      2 @EnableAspectJAutoProxy 会给容器中注册一个组件 AnnotationAwareAspectJAutoProxyCreator
+ *      3 AnnotationAwareAspectJAutoProxyCreator是一个后置处理器
+ *      4 容器创建流程：
+ *          1. registerBeanPostProcessors() 注册后置处理器；创建AnnotationAwareAspectJAutoProxyCreator
+ *          2. finishBeanFactoryInitialization() 初始化剩下的单实例bean
+ *              1.. 创建业务逻辑组件和切面组件
+ *              2.. AnnotationAwareAspectJAutoProxyCreator拦截组件的创建过程
+ *              3.. 组件创建完后，判断组件是否需要增强
+ *                  是：切面的通知方法，包装成增强器(Advisor);给业务逻辑组件创建一个代理对象
+ *      5 执行目标方法：
+ *          1. 代理对象执行目标方法
+ *          2. CglibAopProxy.intercept();
+ *              1.. 得到目标方法拦截器链(增强器包装成拦截器MethodInterceptor)
+ *              2.. 利用拦截器链式机制，依次进入每一个拦截器进行执行；
+ *              3.. 效果
+ *                  正常执行：前置通知-> 目标方法 -> 后置通知 -> 返回通知
+ *                  出现异常：前置通知-> 目标方法 -> 后置通知 -> 异常通知
  *
  *
  *
